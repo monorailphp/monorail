062f3aeaf0ec1aa121a632a700bfd848e17f2edf
Maher El Gamil <maherelgamil@gmail.com>
2026-05-03 23:48:01 +0300

    Test: add InstallCommand test

---
+++ tests/Feature/Commands/InstallCommandTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+it('runs monorail:install command successfully', function () {
+    $exitCode = \Illuminate\Support\Facades\Artisan::call('monorail:install');
+
+    expect($exitCode)->toBe(0);
+});
+
+it('runs monorail:install command and outputs steps', function () {
+    $exitCode = Artisan::call('monorail:install');
+
+    expect($exitCode)->toBe(0);
+});
